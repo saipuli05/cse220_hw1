eaab27c5d6a232f61a1325f48062b9cf3d031c28
saipuli05 <156145913+saipuli05@users.noreply.github.com>
2024-02-15 22:36:58 +0000

    Commit Message goes here

--- src/hw1.c
+++ src/hw1.c
@@ -1,8 +1,52 @@
 #include "hw1.h"
+#include <stdio.h>
+#include <stdint.h>
+
+int32_t bytes(unsigned char one, unsigned char two, unsigned char three, unsigned char four){
+    return (int32_t)(one << 24 | two << 16 | three << 8 | four);
+}
 
 void print_packet_sf(unsigned char packet[])
 {
+    unsigned int srcAddress = (packet[0] << 20) | (packet[1] << 12) | (packet[2] << 4) | (packet[3] >> 4);
+    printf("Source Address: %u\n", srcAddress);
+
+    unsigned int destAddress = ((packet[3] & 0x0F) << 24) | (packet[4] << 16) | (packet[5] << 8) | packet[6];
+    printf("Destination Address: %u\n", destAddress);
+
+    unsigned int srcPort = packet[7] >> 4;
+    printf("Source Port: %u\n", srcPort);
+
+    unsigned int destPort = packet[7] & 0x0F;
+    printf("Destination Port: %u\n", destPort);
+
+    unsigned int fragOffset = (packet[8] << 6) | (packet[9] >> 2);
+    printf("Fragment Offset: %u\n", fragOffset);
+
+    unsigned int packLength = ((packet[9] & 0x03) << 12) | (packet[10] << 4) | (packet[11] >> 4);
+    printf("Packet Length: %u\n", packLength);
+
+    unsigned int maxhopCount = ((packet[11] & 0x0F) << 1) | (packet[12] >> 7);
+    printf("Maximum Hop Count: %u\n", maxhopCount);
+
+    unsigned int checksum = ((packet[12] & 0x7F) << 16) | (packet[13] << 8) | packet[14];
+    printf("Checksum: %u\n", checksum);
+
+    unsigned int compScheme = packet[15] >> 6;
+    printf("Compression Scheme: %u\n", compScheme);
+
+    unsigned int trafClass = packet[15] & 0x3F;
+    printf("Traffic Class: %u\n", trafClass);
+
+    printf("Payload: ");
+    for (unsigned int i = 16; i < packLength; i = i + 4){
+        int32_t payload = bytes(packet[i], packet[i + 1], packet[i + 2], packet[i + 3]);
+        printf("%d ", payload);
+    }
+    printf("\n");
     (void)packet;
+
+    
 }
 
 unsigned int compute_checksum_sf(unsigned char packet[])
